929cfaa81779fc9b12efb539ca48831b6c078c8c
AnanthChavan <ananthchavan2006@gmail.com>
2026-05-24 00:03:20 +0530

    Added Dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landingpage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
   )
