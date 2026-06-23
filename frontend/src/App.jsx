2e9d04cfadc7891e5b7fabb57a07da4e5d17ef89
AnanthChavan <ananthchavan2006@gmail.com>
2026-06-24 00:12:50 +0530

    Added Route for analysis details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
+import AnalysisDetails from './pages/AnalysisDetails.jsx';
 
 export default function App() {
   return (
@@ -32,6 +33,14 @@ export default function App() {
           }
         />
 
+        <Route
+          path="/dashboard/:id"
+          element={
+            <ProtectedRoute>
+              <AnalysisDetails />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
